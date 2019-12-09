51314d4d84d87a7725dcbfc9ff526e89803aefe8
Carlos Molano <926@holbertonschool.com>
2019-12-09 07:04:57 -0500

    Upload

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #create a file
 
 file { 'holberton':
-path => 'tmp/holberton',
-mode => '744',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet',
+  path => 'tmp/holberton',
+  mode => '744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
