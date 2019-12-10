355b2716e35b98a74b508ffa1568479aee24b219
Carlos Molano <926@holbertonschool.com>
2019-12-10 18:27:08 -0500

    Upload 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+#Install nginx, setup the index and setup the redirection
+
+service {'start nginx':
+    ensure  => running,
+    require => Package['nginx'],
+}
+
+package { 'install nginx':
+  ensure => 'installed',
+  name   => 'nginx',
+}
+
+file { 'modify index':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Holberton School',
+}
+
+file_line { 'add the redirection in config file':
+    ensure => 'present',
+    after  => 'server_name _;',
+    path   => '/etc/nginx/sites-available/default',
+    line   => '\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;',
+}
