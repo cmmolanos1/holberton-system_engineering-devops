d6061948c263c010da600f275e07d88eda9217a6
Carlos Molano <926@holbertonschool.com>
2019-12-11 13:27:09 -0500

    FIx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,7 @@
-#!/usr/bin/env bash
-#Install nginx
+#!/usr/bin/env bash                                                                                                                              
+#Install nginx                                                                                                                                   
 
 exec {'Install nginx':
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx',
-  provider => shell,
-}
-
-exec {'Modify index':
-  command  => 'sudo echo "Holberton School" > /var/www/html/index.nginx-debian.html',
-  provider => shell,
-}
-
-exec {'Modify configuration':
-  command  => 'new_string="\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" && sudo sed -i "42i $new_string" /etc/nginx/sites-available/default',
-  provider => shell,
-}
-
-exec {'Restart the server':
-  command  => 'sudo service nginx start',
-  provider => shell,
-}
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo echo "Holberton School" > /var/www/html/index.nginx-debian.html && new_string="\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" && sudo sed -i "42i $new_string" /etc/nginx/sites-available/default && sudo service nginx start',
+    provider => shell,
+    }
