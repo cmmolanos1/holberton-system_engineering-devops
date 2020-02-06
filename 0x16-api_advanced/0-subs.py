f912ca551d3e9955164f0f92babe192b93f2471c
Carlos Molano <926@holbertonschool.com>
2020-02-06 12:31:57 -0500

    Fix condition

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,14 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
     # Change the user agent
-    headers = {'User-Agent': 'cmmolanos'}
-    request = requests.get('https://api.reddit.com/r/{}/about'.
-                           format(subreddit),
-                           headers=headers)
-    subscribers = request.json()
+    try:
+        headers = {'User-Agent': 'cmmolanos'}
+        request = requests.get('https://api.reddit.com/r/{}/about'.
+                               format(subreddit),
+                               headers=headers)
+        subscribers = request.json()
 
-    sub = subscribers['data']['subscribers']
-    return sub
+        sub = subscribers['data']['subscribers']
+        return sub
+    except:
+        return 0
